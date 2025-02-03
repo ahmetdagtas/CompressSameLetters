4f73a122a253e8a85e0fe89d37c1242f358024bc
ahmetdagtas <a.dagtas@gmail.com>
2025-02-03 13:38:51 +0300

    added an unfinished README.md added classes and test classes added coding scenarios

---
+++ README.md
@@ -0,0 +1,4 @@
+The source of the question:
+https://www.linkedin.com/posts/tim-ruscica_before-hiring-microsoft-asks-software-developers-activity-7290739360461717504-lzNG/?utm_medium=ios_app&utm_source=social_share_video_v2&utm_campaign=copy_link
+
+The question is:
--- src/main/java/com/linkedin/compresssameletters/compresser/Compresser.java
+++ src/main/java/com/linkedin/compresssameletters/compresser/Compresser.java
@@ -1,5 +1,33 @@
 package com.linkedin.compresssameletters.compresser;
 
+
+/*
+ * Coding Scenario 1:
+ * The aim is to use first 6 bytes for A-Z 0-9 There is 26+10 = 36 characters is used.
+ * 2^16=65536 -> 65536 - 36 = There is 65500 values left. It is left to us how it is going to be used.
+ * If the character reoccurance amount is beyond 65500's holding capacity,
+ *  the next two byte's first 36 byte will keep the same letter
+ *  the remaining 6500 will be added to the previous value.
+ *  the decompress should be careful about this one, as well.
+ *
+ * Coding Scenario 2:
+ * The aim is to use first 6 bytes for A-Z 0-9 There is 26+10 = 36 characters is used.
+ * Then the remaining 10 bytes to use 2^10 = 1024 (the compressed amount of the recurring)
+ * The recurring will be searched if it can be extended a bigger amount, such as constant multiply pow
+ */
 public class Compresser {
 
+    public String compress(String input) {
+        String output = "A132C";
+        
+        String[] splittedChunks = splitIntoChunks(input);
+        
+        
+        return output;
+    }
+
+
+    private String[] splitIntoChunks(String input) {
+        return null;
+    }
 }
